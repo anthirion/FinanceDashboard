7521f1b0bef23487ec217b7d0af2134db6e5ad01
antoinethirion <antoine.thirion@wavestone.com>
2024-08-09 18:03:20 +0200

    Plot the first line graph

--- README.md
+++ README.md
@@ -8,4 +8,11 @@ This project uses the following tools :
 <li>plotly: Yahoo Finance API used to download historical data on stock markets</li>
 </ul>
 
+# Objectives
+The objectives of this study are :
+<ul>
+<li>Measure the average annual performance of S&P500</li>
+<li>Measure the mean of a stock market crash during big crises (2008, 2020)</li>
+</ul>
+
 # Key takeaways
--- app.py
+++ app.py
@@ -1,29 +1,34 @@
+import yfinance as yf
 from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
+import plotly.express as px
+
+from typing import Dict
 
 app = Dash(__name__)
 
+# get historical data of S&P 500 from Yahoo API
+historical_data: dict = yf.Ticker("^GSPC").history(period='5d')
 
 app.layout = html.Div([
-    html.H4('Interactive color selection with simple Dash example'),
-    html.P("Select color:"),
+    html.H1("Historical tock prices of S&P 500"),
+    dcc.Graph(id="time-series-chart"),
+    html.P("Select period:"),
     dcc.Dropdown(
-        id="dropdown",
-        options=['Gold', 'MediumTurquoise', 'LightGreen'],
-        value='Gold',
+        id="period",
+        options=historical_data["validRanges"],
+        value="5d",
         clearable=False,
     ),
-    dcc.Graph(id="graph"),
 ])
 
 
 @app.callback(
-    Output("graph", "figure"),
-    Input("dropdown", "value"))
-def display_color(color):
-    fig = go.Figure(
-        data=go.Bar(y=[2, 3, 1],  # replace with your own data source
-                    marker_color=color))
+    Output(component_id="time-series-chart", component_property="figure"),
+    Input(component_id="period", component_property="value"))
+def display_time_series(input_period):
+    df = historical_data["tradingPeriods"]
+    fig = px.line(df, x='start', y='end')
     return fig
 
 
